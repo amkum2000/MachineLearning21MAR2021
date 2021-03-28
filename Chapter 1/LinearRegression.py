fc021dcd5cacb819a19a4c171af58b7f78441fdb
Amit Kumar <amit.kumarc2.1208@gmail.com>
2021-03-28 18:56:37 -0400

    Add 2nd file and changes to file 1

--- Chapter 1/LinearRegression.py
+++ Chapter 1/LinearRegression.py
@@ -6,6 +6,28 @@ import seaborn as sns
 import sklearn
 import html
 
-data = pd.read_csv("D:/OneDrive/Documents/101 Online Learning/101 Udemy/machine-learning-101-with-scikit-learn-and-stats-models/S3_L17/real_estate_price_size.csv")
+data = pd.read_csv(
+    "D:/OneDrive/Documents/101 Online Learning/101 Udemy/machine-learning-101-with-scikit-learn-and-stats-models/S3_L16/1.01. Simple linear regression.csv")
 print(data.head(10))
 print(data.describe())
+
+Y = data['GPA']
+X = data['SAT']
+print(Y.head())
+print(X.head())
+plt.scatter(X, Y)
+plt.xlabel('SAT', fontsize=20)
+plt.ylabel('GPA', fontsize=20)
+# plt.show()
+
+x1 = sm.add_constant(X)
+results = sm.OLS(Y, x1).fit()
+print(results.summary())
+yhat = 0.2750 + 0.0017 * X
+yhat1 = 0 + 0.0017 * 1850
+
+print(yhat1)
+
+# plt.scatter(X,yhat, c='Orange',lw=4)
+fig = plt.plot(X, yhat, c='Red', lw=9, label='Linear Regression')
+plt.show()
